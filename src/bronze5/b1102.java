1aa8b00c3ea59e6284077fe2d36616b174ffae94
home <viostarryy@gmail.com>
2023-11-02 13:55:12 +0900

    start

---
+++ src/bronze5/b1102.java
@@ -0,0 +1,10 @@
+package bronze5;
+
+public class b1102 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author dbdbd
+ *
+ */
+module CodingTest {
+}
