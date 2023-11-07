abc8141b1eb93055c9efb52b48626fccd3cf43d7
siirro <103366296+siirro@users.noreply.github.com>
2023-11-07 18:37:45 +0900

    [level unrated] Title: 카운트 업, Time: 0.03 ms, Memory: 69.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -1,16 +1,14 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int start_num, int end_num) {
-        int[] answer = {};
+    public List<Integer> solution(int start_num, int end_num) {
+        
         List<Integer> ar = new ArrayList<>();
         
         for(int i=start_num; i<=end_num; i++) {
             ar.add(i);
         }
         
-        answer = ar.stream().mapToInt(Integer::intValue).toArray();
-        
-        return answer;
+        return ar;
     }
 }
