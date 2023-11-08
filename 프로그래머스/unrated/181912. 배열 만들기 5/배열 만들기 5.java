1eadc2f1b5c60d9705ef422cb099fbcd7da371ae
siirro <103366296+siirro@users.noreply.github.com>
2023-11-08 20:20:53 +0900

    Update 배열 만들기 5.java

--- 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.java
+++ 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.java
@@ -6,16 +6,12 @@ class Solution {
         int cut = 0;
 
             for(int i=0; i<intStrs.length;i++) {
-                if(s+l<=intStrs[i].length()&&intStrs[i].length()<=120)
                 cut = Integer.parseInt(intStrs[i].substring(s, s+l));
                 if(cut>k) {
                     ar.add(cut);
                 }
             }
-        
-            
-            
-            
+
         return ar;
     }
 }
