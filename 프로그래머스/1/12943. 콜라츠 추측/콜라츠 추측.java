dc42bf55ea3497d1059e17f67d68464c71b9eabe
siirro <103366296+siirro@users.noreply.github.com>
2023-12-16 13:56:08 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -3,18 +3,15 @@ class Solution {
         int answer = 0;
                 
         while(num!=1) {
-            
+            answer++;
             if(answer>500) {
                 return -1;        
             }
-
             if(num%2==0) {
                 num /= 2;
             } else {
                 num = num*3+1;
             }
-            answer++;
-
         }
         
         return answer;
