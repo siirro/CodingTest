f336128e461f5a6a49f40f155c3eacfdff037880
siirro <103366296+siirro@users.noreply.github.com>
2023-12-16 14:09:56 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 6.12 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,16 +1,24 @@
+import java.util.Arrays;
+
 class Solution {
     public String solution(String[] seoul) {
         // 어레이리스트 안쓰고 반복문으로 찾을게
-        int index = 0;
+//         int index = 0;
         
-        for(String example : seoul) {
-            if("Kim".equals(example)) {
-                break;
-            }
-            index++;
-        }
+//         for(String example : seoul) {
+//             if("Kim".equals(example)) {
+//                 break;
+//             }
+//             index++;
+//         }
         
+//         String answer = "김서방은 "+index+"에 있다";
+        
+        
+        // 어레이쓸게
+        int index = Arrays.asList(seoul).indexOf("Kim");
         String answer = "김서방은 "+index+"에 있다";
         return answer;
     }
+
 }
