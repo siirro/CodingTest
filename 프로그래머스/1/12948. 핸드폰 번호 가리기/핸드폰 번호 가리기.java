b0c5ca3c9f6a6b0605e8bfca0fdab196f49145aa
siirro <103366296+siirro@users.noreply.github.com>
2023-12-16 15:31:29 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 2.05 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -2,17 +2,15 @@ class Solution {
     public String solution(String phone_number) {
         String answer = "";
         
-        // 뒤 4자리를 제외한 전화번호를 잘라서 sub에 저장
-        String sub = phone_number.substring(0, phone_number.length()-4);
-        
-        // sub의 길이만큼의 별 생성
+        // 뒤 4자 제외한 길이만큼의 별 생성
         String star = "";
-        for(int i=0; i<sub.length(); i++) {
+        for(int i=0; i<phone_number.length()-4; i++) {
             star += "*";
         }
+
+        // star와 뒤 4자를 붙임
+        star += phone_number.substring(phone_number.length()-4, phone_number.length());
+        return star;
         
-        // sub을 star로 치환
-        answer = phone_number.replace(sub, star);
-        return answer;
     }
 }
