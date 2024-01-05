85082864092d48244d03868396419028adc2d95b
siirro <103366296+siirro@users.noreply.github.com>
2024-01-06 00:45:23 +0900

    Time: 41 ms (44.36%), Space: 17.1 MB (22.70%) - LeetHub

--- LeetCode/Easy/0771-jewels-and-stones/0771-jewels-and-stones.py
+++ LeetCode/Easy/0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,8 +1,19 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
         count = 0
-        for i, jew in enumerate(jewels):
-            for j, stn in enumerate(stones):
-                if jew == stn: count += 1
+        
+        # 1. 그냥 반복문으로 풀기
+        # for i, jew in enumerate(jewels):
+        #     for j, stn in enumerate(stones):
+        #         if jew == stn: count += 1
 
+        # 2. 딕셔너리를 이용해 풀기 : 모든 스톤의 개수를 세고, 스톤 키값에 쥬얼리를 하나씩 넣은 밸류값만 더해주면 된다.
+        dic_stones = defaultdict(int)
+        
+        for i, n in enumerate(stones):
+            dic_stones[n] += 1 
+        
+        for i, n in enumerate(jewels):
+            count += dic_stones[n]
+        
         return count
