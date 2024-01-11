c72087bfe0262feea85eb29a8e14d5dc47e45104
siirro <103366296+siirro@users.noreply.github.com>
2024-01-11 09:53:01 +0900

    Time: 40 ms (61.20%), Space: 17.3 MB (36.84%) - LeetHub

--- LeetCode/Medium/0078-subsets/0078-subsets.py
+++ LeetCode/Medium/0078-subsets/0078-subsets.py
@@ -2,10 +2,10 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         answer = []
                 
-        def dfs(start, arr):
-            answer.append(arr)
+        def dfs(start, list):
+            answer.append(list)
             for i in range(start, len(nums)):
-                dfs(i+1, arr+[nums[i]])
+                dfs(i+1, list+[nums[i]])
                 
         dfs(0, [])
         return answer
